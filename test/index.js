2d000b1ad378504d22cf0ba8fd9b21047ad0c7c5
passakorn <non.ty123@hotmail.com>
2020-09-08 02:56:04 +0700

    update Sunburst

---
+++ test/index.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const path = require('path')
+//const test = require('./zoomablesunburst.js')
+const app = express()
+const Vgen = require('./vgenTest.js')
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/test.htm'));
+})
+app.get('/zoomablesunburst.js', (req, res) => {
+  res.sendFile(path.join(__dirname + '/zoomablesunburst.js'));
+  //res.send(test.chart())
+  //console.log(test.chart().style)
+})
+// //console.log(test.a());
+app.get('/test.js', (req, res) => {
+  res.sendFile(path.join(__dirname + '/test.js'));
+})
+app.get('/vgenTest.js', (req, res) => {3
+  var sunburst = Vgen.Vgen.createSunburst()
+  res.send(sunburst.generate())
+})
+app.listen(80, () => {
+  //console.log(Vgen.Vgen.createSunburst().genConfig('del','abc',{'name2':'Aunaun'}))
+  console.log('Start server at port 80.')
+})
+
