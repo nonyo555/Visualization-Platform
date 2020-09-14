550224c57b466ba36aa5061af9b86dbd43744e76
passakorn <non.ty123@hotmail.com>
2020-09-15 03:00:02 +0700

    addPolygonClasstoVgen

--- charts/Vgenerate.js
+++ charts/Vgenerate.js
@@ -1,4 +1,5 @@
 const sunburst = require('./ZoomableSunburst/classSunburst.js')
+const polygon = require('./ThaiPolygon/classThaiPolygon.js')
 class Vgen{
     graphs = [1,2,3,4,5] 
     static getGrap(){
@@ -8,9 +9,9 @@ class Vgen{
     {
         return new sunburst.Sunburst()
     }
-    // static createThaiPolygon()
-    // {
-    //     return new ThaiPolygon()
-    // }
+    static createThaiPolygon()
+    {
+        return new polygon.ThaiPolygon()
+    }
 }
 module.exports ={Vgen}
